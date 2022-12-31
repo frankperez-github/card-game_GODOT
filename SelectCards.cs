9c9b837697afc785fda0ffa8efd516fd6d6029f0
0HH02 <105432115+0HH02@users.noreply.github.com>
2022-12-31 14:09:22 -0500

    ...

--- SelectCards.cs
+++ SelectCards.cs
@@ -9,18 +9,6 @@ namespace gameVisual
         public static Button AcceptButton;
         Button Left;
         Button Right;
-<<<<<<< HEAD
-            public static int selectQuant = 1;
-            public static List<Sprite> selectCards;
-            public static Action<List<Relics>, int> SelectDelegate;
-            public static InterpretAction action;
-            public static Label SelectLabel;
-            public static PackedScene SelectCardsScene = (PackedScene)GD.Load("res://SelectCards.tscn");
-            public static Node SelectCardInstance = SelectCardsScene.Instance(); 
-            public static Dictionary<int, List<int>> SelectedIndexes = new Dictionary<int, List<int>>();
-
-            public static List<Relics> Source;
-=======
         public static int selectQuant = 1;
         public static List<Sprite> selectCards;
         public static Action<List<Relics>, int> SelectDelegate;
@@ -31,7 +19,6 @@ namespace gameVisual
         public static Dictionary<int, List<int>> SelectedIndexes = new Dictionary<int, List<int>>();
 
         public static List<Relics> Source;
->>>>>>> 8b7ae770f5b3bbb8db4a92b528a14f3b541f8bd5
         public static int actualSwipe = 0;
         public static int partitions = 0;
         public static string selectName;
@@ -88,18 +75,10 @@ namespace gameVisual
 
             if(Left.Pressed)
             {
-<<<<<<< HEAD
-                this.QueueFree();
-=======
->>>>>>> 8b7ae770f5b3bbb8db4a92b528a14f3b541f8bd5
                 VisualMethods.SetSelectCardsProperties(selectName, partitions, actualSwipe-1, Source, selectQuant);
             }
             if(Right.Pressed)
             {
-<<<<<<< HEAD
-                this.QueueFree();
-=======
->>>>>>> 8b7ae770f5b3bbb8db4a92b528a14f3b541f8bd5
                 VisualMethods.SetSelectCardsProperties(selectName, partitions, actualSwipe+1, Source, selectQuant);
             }
 
@@ -127,12 +106,9 @@ namespace gameVisual
                                         SelectedIndexes[actualSwipe] = updatedIndexes;
 
                                         selectQuant++;
-<<<<<<< HEAD
-                                        VisualMethods.SelectedCards.Remove(SelectCards.Source[i]);
-=======
                                         VisualMethods.SelectedCards.Remove(SelectCards.Source[i+actualSwipe*VisualMethods.partitionLength]);
->>>>>>> 8b7ae770f5b3bbb8db4a92b528a14f3b541f8bd5
                                         selectCards[i].Scale = new Vector2((float)0.170,(float)0.170);
+                                        GD.Print(String.Join(" ", SelectedIndexes[actualSwipe]));
                                     }
                                     // Selecting
                                     else if (selectQuant != 0)
@@ -143,12 +119,10 @@ namespace gameVisual
                                         SelectedIndexes[actualSwipe] = updatedIndexes;
 
                                         selectQuant--;
-<<<<<<< HEAD
-                                        VisualMethods.SelectedCards.Add(SelectCards.Source[i]);
-=======
                                         VisualMethods.SelectedCards.Add(SelectCards.Source[i+actualSwipe*VisualMethods.partitionLength]);
->>>>>>> 8b7ae770f5b3bbb8db4a92b528a14f3b541f8bd5
                                         selectCards[i].Scale = new Vector2((float)0.185,(float)0.185);
+                                        GD.Print(String.Join(" ", SelectedIndexes[actualSwipe]));
+
                                     }
                                     break;
                                 }
